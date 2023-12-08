99cbbba4809e99afcecb70cee0ca8357a702bc1e
Rain <emeraldsnorlax@disroot.org>
2023-12-08 12:39:51 +0000

    init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<script>
+	import '../app.pcss';
+	import 'tailwindcss/src/css/preflight.css'
+	import '@fontsource/mansalva';
+	import '@fontsource/cutive';
+</script>
+
+<slot />
---
+++ src/routes/articles/+layout.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+  import "./article.pcss"
+</script>
+
+<slot />
