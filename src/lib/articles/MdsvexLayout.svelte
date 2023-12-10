1f6d1608810316e98a71d79ddefea2d3708b39d9
Rain <emeraldsnorlax@disroot.org>
2023-12-10 14:28:22 +0000

    move <a> styles to a component

--- src/lib/articles/MdsvexLayout.svelte
+++ src/lib/articles/MdsvexLayout.svelte
@@ -1,7 +1,8 @@
 <script context="module" lang="ts">
 	import h1 from './custom/h1.svelte';
 	import p from './custom/p.svelte';
-	export { h1, p };
+	import a from './custom/a.svelte';
+	export { h1, p, a };
 </script>
 
 
---
+++ src/lib/articles/custom/a.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+	export let href: string;
+</script>
+
+<a {href} class="text-ink dark:text-chalk font-extrabold underline underline-offset-4 decoration-wavy decoration-1 decoration-ink-faded dark:decoration-chalk-faded hover:opacity-90 transition-opacity"><slot /></a>
