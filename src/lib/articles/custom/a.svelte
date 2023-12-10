ececbdb61b4c81e9e310bc94f62e7285b6252d0b
Rain <emeraldsnorlax@disroot.org>
2023-12-10 22:18:39 +0000

    bold looks silly

--- src/lib/articles/custom/a.svelte
+++ src/lib/articles/custom/a.svelte
@@ -2,4 +2,4 @@
 	export let href: string;
 </script>
 
-<a {href} class="text-ink dark:text-chalk font-extrabold underline underline-offset-4 decoration-wavy decoration-1 decoration-ink-faded dark:decoration-chalk-faded hover:opacity-90 transition-opacity"><slot /></a>
+<a {href} class="text-ink dark:text-chalk underline underline-offset-4 decoration-wavy decoration-1 decoration-ink-faded dark:decoration-chalk-faded hover:opacity-90 transition-opacity"><slot /></a>
